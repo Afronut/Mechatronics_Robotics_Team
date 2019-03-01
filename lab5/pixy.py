9debc1722b302bdb742098a2fe8ab6e250692036
Coovi MEHA <43079588+CooviM@users.noreply.github.com>
2019-03-01 10:35:18 -0700

    Update pixy.py

--- lab5/pixy.py
+++ lab5/pixy.py
@@ -11,7 +11,7 @@ DEVICE_ADDRESS = 0x54
 
 while True:
     word = bus.read_byte_data(DEVICE_ADDRESS, 0)
-    print hex(word)
+    print 'word2 is {}'.format(hex(word))
     word1 = bus.read_byte_data(DEVICE_ADDRESS, 1)
     print 'word1 is {}'.format(hex(word1))
     time.sleep(0.1)
