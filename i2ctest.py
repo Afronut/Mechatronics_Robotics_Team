2d2d49b3bbdc3eb24391e6871270abcccd928421
Coovi MEHA <43079588+CooviM@users.noreply.github.com>
2019-03-01 10:21:42 -0700

    Update i2ctest.py

--- i2ctest.py
+++ i2ctest.py
@@ -9,17 +9,17 @@ DEVICE_ADDRESS = 0x54
 
 
 while True:
-    word = bus.read_word_data(DEVICE_ADDRESS, 0)
+    word = bus.read_word_data(DEVICE_ADDRESS, 1)
     print hex(word)
     if word == 0xaa55:
-        word = bus.read_word_data(DEVICE_ADDRESS, 0)
+        word = bus.read_word_data(DEVICE_ADDRESS, 1)
         if word == 0xaa56:
-            checksum = bus.read_word_data(DEVICE_ADDRESS, 0)
-            signature = bus.read_word_data(DEVICE_ADDRESS, 0)
-            xctr = bus.read_word_data(DEVICE_ADDRESS, 0)
-            yctr = bus.read_word_data(DEVICE_ADDRESS, 0)
-            width = bus.read_word_data(DEVICE_ADDRESS, 0)
-            height = bus.read_word_data(DEVICE_ADDRESS, 0)
+            checksum = bus.read_word_data(DEVICE_ADDRESS, 1)
+            signature = bus.read_word_data(DEVICE_ADDRESS, 1)
+            xctr = bus.read_word_data(DEVICE_ADDRESS, 1)
+            yctr = bus.read_word_data(DEVICE_ADDRESS, 1)
+            width = bus.read_word_data(DEVICE_ADDRESS, 1)
+            height = bus.read_word_data(DEVICE_ADDRESS, 1)
             print "signature: ", signature
             print "x ctr: ", xctr
             print "y ctr: ", yctr
