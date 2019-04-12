0fc8328a54d8df20bfbc81d95074b2c0ddae043b
coovi <coovi.meha@colorado.edu>
2019-04-12 09:49:21 -0600

    update client.py

--- roomba/client.py
+++ roomba/client.py
@@ -2,7 +2,7 @@ import socket
 
 HOST = '10.201.92.66'  # Enter IP or Hostname of your server
 # Pick an open Port (1000+ recommended), must match the server port
-PORT = 12345
+PORT = 12345 #new comment
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
