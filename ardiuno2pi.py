53a5d917e9283c35656c7468c170fcb15d8a3b7a
Coovi MEHA <43079588+CooviM@users.noreply.github.com>
2019-04-16 17:33:40 -0600

    Update ardiuno2pi.py

--- ardiuno2pi.py
+++ ardiuno2pi.py
@@ -2,7 +2,7 @@ from serial import Serial
 from time import sleep
 ser = Serial('/dev/ttyUSB0', 9600)
 
-line = ser.write('up')
+line = ser.write(b'up')
 sleep(1)
 while True:
   line= ser.readline()
