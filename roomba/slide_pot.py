b33851e1b8d6f5c41a4dfa3439ae12310a7742c1
Coovi MEHA <43079588+CooviM@users.noreply.github.com>
2019-04-23 23:54:45 -0600

    Update slide_pot.py

--- roomba/slide_pot.py
+++ roomba/slide_pot.py
@@ -19,49 +19,49 @@ GPIO.setup(18, GPIO.OUT) # Connected to PWMA
 # Thread(target=run_server).start()
   
 
-def forward():
-  pwm = GPIO.PWM(18, 1000)
-  # Drive the motor clockwise
-  GPIO.output(6, GPIO.HIGH) # Set AIN1
-  GPIO.output(5, GPIO.LOW) # Set AIN2
+# def forward():
+pwm = GPIO.PWM(18, 1000)
+# Drive the motor clockwise
+GPIO.output(6, GPIO.HIGH) # Set AIN1
+GPIO.output(5, GPIO.LOW) # Set AIN2
 
-  # Set the motor speed
-  # GPIO.output(18, GPIO.HIGH) # Set PWMA
-  pwm.start(50)
+# Set the motor speed
+# GPIO.output(18, GPIO.HIGH) # Set PWMA
+pwm.start(50)
 
-  # Disable STBY (standby)
-  GPIO.output(13, GPIO.HIGH)
+# Disable STBY (standby)
+GPIO.output(13, GPIO.HIGH)
 
-  # Wait 5 seconds
-  time.sleep(25)
+# Wait 5 seconds
+time.sleep(25)
 # GPIO.output(18, GPIO.LOW) # Set PWMA
 # time.sleep(2.5)
 # # pwm = GPIO.PWM(18, 1000)
 # pwm.start(50)
-  pwm.stop()
-  GPIO.output(6, GPIO.LOW) # Set AIN1
-  GPIO.output(5, GPIO.LOW) # Set AIN2
-  pwm.stop()
-  GPIO.output(13, GPIO.LOW) # Set STBY
-  GPIO.cleanup()  
-
-  
-def backward():
-  pwm = GPIO.PWM(18, 1000)
-  # Drive the motor counterclockwise
-  print "Counterclock"
-  GPIO.output(6, GPIO.LOW) # Set AIN1
-  GPIO.output(5, GPIO.HIGH) # Set AIN2
-  pwm.start(50)
+pwm.stop()
+GPIO.output(6, GPIO.LOW) # Set AIN1
+GPIO.output(5, GPIO.LOW) # Set AIN2
+pwm.stop()
+GPIO.output(13, GPIO.LOW) # Set STBY
+GPIO.cleanup()  
 
 
-  # Wait 5 seconds
-  time.sleep(25)
+# def backward():
+pwm = GPIO.PWM(18, 1000)
+# Drive the motor counterclockwise
+print "Counterclock"
+GPIO.output(6, GPIO.LOW) # Set AIN1
+GPIO.output(5, GPIO.HIGH) # Set AIN2
+pwm.start(50)
 
-  # Reset all the GPIO pins by setting them to LOW
-  GPIO.output(6, GPIO.LOW) # Set AIN1
-  GPIO.output(5, GPIO.LOW) # Set AIN2
-  pwm.stop()
-  GPIO.output(13, GPIO.LOW) # Set STBY
-  GPIO.cleanup()  
+
+# Wait 5 seconds
+time.sleep(25)
+
+# Reset all the GPIO pins by setting them to LOW
+GPIO.output(6, GPIO.LOW) # Set AIN1
+GPIO.output(5, GPIO.LOW) # Set AIN2
+pwm.stop()
+GPIO.output(13, GPIO.LOW) # Set STBY
+GPIO.cleanup()  
 
