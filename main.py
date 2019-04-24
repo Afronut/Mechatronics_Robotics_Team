5921b0c01ff16191039d1b723ec1c093ee517dbb
rpwaugh6 <47930263+rpwaugh6@users.noreply.github.com>
2019-04-24 10:23:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from time import sleep
 from roomba.map import*
 
 Thread(target=run_server).start()
-# assignement = None
-# try:
-#     arduino = SerialManager(device='/dev/ttyACM0')
-# except:
-#     pass
+assignement = None
+try:
+    arduino = SerialManager(device='/dev/ttyACM0')
+except:
+    pass
 
 while True:
     # Import required modules
@@ -25,24 +25,24 @@ while True:
      #   print(message)
     # else:
    #     print('no message yet')
-    # sleep(2)
-    # if not assignement:
-    #     assignement = barcode_funder()[0]
-    #     print(assignement)
-    # sleep(4)
+    sleep(2)
+    if not assignement:
+        assignement = barcode_funder()[0]
+        print(assignement)
+    sleep(4)
     #     pallet, rack, row, col,dock=rack_finder(assignement)
     #     code=barcode_funder()[0]
     #     start=floor_finder(code)
     #     end=rack['rack_id']
     #     path_to_take=path_finder(start[1],end)
     sleep(2)
-#     arduino.write("go_straight")
+    arduino.write("go_straight")
 # #     message=arduino.readline()
 # #     if message:
 # #         print (message)
 # #     else:
 # #         print('receive no message')
-#     while True:
-#         code = barcode_funder()[0]
-#         print(code)
-#         sleep(4)
+    while True:
+        code = barcode_funder()[0]
+        print(code)
+        sleep(4)
